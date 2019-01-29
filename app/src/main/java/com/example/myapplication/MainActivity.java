c1e7c456520d8329d0770bcc8c4cc597b18ecedc
bugger-debugging <33279107+Khali851999@users.noreply.github.com>
2019-01-29 21:03:02 +0530

    2nd commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,10 +6,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 class MainActivity extends AppCompatActivity {
-
+Animation bounce;
+ImageView bounceimage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +23,11 @@ class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+//ANIMATION
+        bounceimage=findViewById(R.id.bounceimage);
+        bounce= AnimationUtils.loadAnimation(MainActivity.this,R.anim.mainpagebounce);
+        bounceimage.startAnimation(bounce);
 
 //SETTING FLASH SCREEN
         LogoLauncher logoLauncher=new LogoLauncher();
@@ -32,7 +41,7 @@ class MainActivity extends AppCompatActivity {
             }catch(InterruptedException e){
                 e.printStackTrace();
             }
-            Intent i=new Intent(MainActivity.this,Main2Activity.class);
+            Intent i=new Intent(MainActivity.this,mainpage.class);
             startActivity(i);
 
 //PREVENTING FROM OPENING FLASH SCREEN ON BACK PRESS(DESTROYING THE ACTIVITY)
--- app/src/main/java/com/example/myapplication/mainpage.java
+++ app/src/main/java/com/example/myapplication/mainpage.java
@@ -1,31 +1,96 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
-public class mainpage extends AppCompatActivity {
-    ImageView i1;
-    Animation rotate,scale,trans,alpha;
+public class mainpage extends AppCompatActivity implements Animation.AnimationListener {
+    ImageView i1,i2,i3,i4;
+    LinearLayout ll1,ll2,ll3,ll4;
+    Animation rotate,scale,scale1,trans,alpha;
+    Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_mainpage);
-        i1=findViewById(R.id.i1);
-//        i2=findViewById(R.id.i2);
-//        i3=findViewById(R.id.i3);
-//        i4=findViewById(R.id.i4);
-        scale= AnimationUtils.loadAnimation(mainpage.this,R.anim.scale_anim);
+        getSupportActionBar().hide();
 
-        rotate= AnimationUtils.loadAnimation(mainpage.this,R.anim.rotate_anim);
-//        i2.startAnimation(rotate);
-        trans= AnimationUtils.loadAnimation(mainpage.this,R.anim.trans_anim);
-//        i3.startAnimation(trans);
+        i1=findViewById(R.id.i1);
+        i2=findViewById(R.id.i2);
+        i3=findViewById(R.id.i3);
+        i4=findViewById(R.id.i4);
+        ll1=findViewById(R.id.ll1);
+        ll2=findViewById(R.id.ll2);
+        ll3=findViewById(R.id.ll3);
+        ll4=findViewById(R.id.ll4);
+
+        scale= AnimationUtils.loadAnimation(mainpage.this,R.anim.scale_anim);
+        scale1= AnimationUtils.loadAnimation(mainpage.this,R.anim.scale1_anim);
         alpha= AnimationUtils.loadAnimation(mainpage.this,R.anim.alpha_anim);
-//        i4.startAnimation(alpha);
-        i1.startAnimation(scale);
+        ll1.startAnimation(scale);
+        ll2.startAnimation(scale);
+        ll3.startAnimation(scale);
+        ll4.startAnimation(scale);
+        ll1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                ll1.startAnimation(scale1);
+
+                return false;
+            }
+        });
+
+
+
+    }
+    public void vv(View view){
+
+        switch (view.getId()) {
+            case R.id.ll1:
+                i1.startAnimation(alpha);
+                alpha.setAnimationListener(this);
+                break;
+            case R.id.ll2:
+                i2.startAnimation(alpha);
+                alpha.setAnimationListener(this);
+                break;
+            case R.id.ll3:
+                i3.startAnimation(alpha);
+                alpha.setAnimationListener(this);
+                break;
+            case R.id.ll4:
+                i4.startAnimation(alpha);
+                alpha.setAnimationListener(this);
+                break;
+
+        }
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+    @Override
+    public void onAnimationEnd(Animation animation)
+{
+    Intent intent1=new Intent(mainpage.this,Main2Activity.class);
+    startActivity(intent1);
+
+}
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
     }
 }
